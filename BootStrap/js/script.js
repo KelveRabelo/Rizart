cf9868b706be90ea145ca2af17fd8998ca13e6f9
Kelve Rabelo <kelve.r.farias@hotmail.com>
2022-11-04 15:46:21 -0300

    up

--- BootStrap/js/script.js
+++ BootStrap/js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
 
     function animationData() {
         let duration = [1400, 1600, 2000, 2200];
-        let number = [60, 254, 32, 5243];
+        let number = [60, 254, 3, 5243];
         let c = 65;
         for (let toCircle = 0; toCircle < 4; toCircle++){
 
@@ -43,5 +43,81 @@ $(document).ready(function () {
     // parallax
     setTimeout(function() {
         $("#data-area").parallax({imageSrc: 'img/cidadeparallax.png'});
+        $("#apply-area").parallax({imageSrc: 'img/pattern.png'});
     }, 250);
+
+
+     // Filtro portfólio
+
+  $('.filter-btn').on('click', function() {
+
+    let type = $(this).attr('id');
+    let boxes = $('.project-box');
+
+    $('.main-btn').removeClass('active');
+    $(this).addClass('active');
+
+    if(type == 'dsg-btn') {
+      eachBoxes('dsg', boxes);
+    } else if(type == 'dev-btn') {
+      eachBoxes('dev', boxes);
+    } else if(type == 'seo-btn') {
+      eachBoxes('seo', boxes);
+    } else {
+      eachBoxes('all', boxes);
+    }
+
+  });
+
+  function eachBoxes(type, boxes) {
+
+    if(type == 'all') {
+      $(boxes).fadeIn();
+    } else {
+      $(boxes).each(function() {
+        if(!$(this).hasClass(type)) {
+          $(this).fadeOut('slow');
+        } else {
+          $(this).fadeIn();
+        }
+      });
+    }
+  }
+
+  // scroll para as seções
+
+  let navBtn = $('.nav-item');
+
+  let bannerSection = $('#mainSlider');
+  let aboutSection = $('#about-area');
+  let servicesSection = $('#services-area');
+  let teamSection = $('#team-area');
+  let portfolioSection = $('#portfolio-area');
+  let contactSection = $('#contact-area');
+
+  let scrollTo = '';
+
+  $(navBtn).click(function() {
+
+    let btnId = $(this).attr('id');
+
+    if(btnId == 'about-menu') {
+      scrollTo = aboutSection;
+    } else if(btnId == 'services-menu') {
+      scrollTo = servicesSection;
+    } else if(btnId == 'team-menu') {
+      scrollTo = teamSection;
+    } else if(btnId == 'portfolio-menu') {
+      scrollTo = portfolioSection;
+    } else if(btnId == 'contact-menu') {
+      scrollTo = contactSection;
+    } else {
+      scrollTo = bannerSection;
+    }
+
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $(scrollTo).offset().top - 70
+    }, 1500);
+  });
+
 });
